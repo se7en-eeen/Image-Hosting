b3497c3ec66d0d1116928c1a77fda0a1dba81c9a
se7en <se7en.eeen@gmail.com>
2019-05-22 15:30:19 +0800

    试试手气 完善 & 签到检查封装

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,12 +12,12 @@ class IndexController extends Controller {
         $data_sum = M("data")->where($where)->count();
         $page = new Page($data_sum, 15);
         $file_list = M("data")->where($where)->limit($page->firstRow, $page->listRows)->order("id desc")->select();
-        $check_in = M("check_in")->where(array("user_id" => $where['img_user'], "check_in_time" => array(array('egt', strtotime(date("Y-m-d 0:0:0"))), array('elt', strtotime(date("Y-m-d 23:59:59"))), 'and')))->find();
+        $check_in = $this->checkInCheck($where['img_user']);
         $this->assign("file_list", $file_list);
         $this->assign("page", $page->show());
         $this->assign("action_name", $action_name);
         $this->assign("title", $title);
-        $this->assign("check_in", ($check_in) ? 1 : 0);
+        $this->assign("check_in", $check_in);
         $this->display($display_page);
     }
 
@@ -124,6 +124,11 @@ class IndexController extends Controller {
         }
     }
 
+    public function checkInCheck($user_id) {
+        $check_in = M("check_in")->where(array("user_id" => $user_id, "check_in_time" => array(array('egt', strtotime(date("Y-m-d 0:0:0"))), array('elt', strtotime(date("Y-m-d 23:59:59"))), 'and')))->find();
+        return ($check_in) ? 1 : 0;
+    }
+
     public function updateStatus() {
         $update_result = M("data")->where(array("id" => I("post.id")))->setField('img_status', (I("post.status") == "1") ? "0" : "1");
         $this->ajaxReturn($update_result);
@@ -137,24 +142,49 @@ class IndexController extends Controller {
     }
 
     public function lucky() {
+        $login_status = $this->loginCheck();
+        $lucky_log_has = M("lucky_log")->where(array("user_id" => $login_status['user_id']))->find();
+        if ($lucky_log_has) {
+            $lucky_log_time = $lucky_log_has['last_time'] >= strtotime(date("Y-m-d 00:00:00")) && $lucky_log_has['last_time'] <= strtotime(date("Y-m-d 23:59:59"));
+            if ($lucky_log_has['lucky_sum'] > 10 || $lucky_log_time || $lucky_log_has['last_time'] < time() + (30 * 60)) {
+                $query = M("data")->where(array("id" => $lucky_log_has['lucky_id']))->select();
+            }
+        }
+
+        if (!isset($query)){
+            $query = $this->lucky_random();
+            if ($lucky_log_has) {
+                $lucky_log_has['lucky_id'] = $query[0]['id'];
+                $lucky_log_has['last_time'] = time();
+                $lucky_log_has['lucky_sum'] += 1;
+                $lucky_result = M("lucky_log")->save($lucky_log_has);
+            } else {
+                $lucky_result = M("lucky_log")->add(array(
+                    "user_id" => $login_status['user_id'],
+                    "lucky_id" => $query[0]['id'],
+                    "last_time" => time(),
+                    "lucky_sum" => 1
+                ));
+            }
+            if (!$lucky_result) $this->error("出错了", "Index/index");
+        }
+
+        $check_in = $this->checkInCheck($login_status['user_id']);
+        $this->assign("file_list", $query);
+        $this->assign("action_name", "lucky");
+        $this->assign("title", "试试手气");
+        $this->assign("check_in", $check_in);
+        $this->display("Index/index");
+
+    }
+
+    public function lucky_random() {
         $between = M("data")->field("MIN(id) as min, MAX(id) as max, COUNT(1) as count")->find();
-        $where = array(
-            "id" => rand($between['min'], $between['max']),
-            "img_status" => 0
-        );
+        $where = array("id" => rand($between['min'], $between['max']), "img_status" => 0);
         $roll = rand(0, 100);
         if ($roll <= 5) $where['img_status'] = 1;
         $query = M("data")->where($where)->select();
-        if (empty($query)) $this->lucky();
-        else {
-            $login_status = $this->loginCheck();
-            $check_in = M("check_in")->where(array("user_id" => $login_status['user_id'], "check_in_time" => array(array('egt', strtotime(date("Y-m-d 0:0:0"))), array('elt', strtotime(date("Y-m-d 23:59:59"))), 'and')))->find();
-            $this->assign("file_list", $query);
-            $this->assign("action_name", "lucky");
-            $this->assign("title", "试试手气");
-            $this->assign("check_in", ($check_in) ? 1 : 0);
-            $this->display("Index/index");
-        }
+        return (empty($query)) ? $this->lucky_random() : $query;
     }
 
     public function checkIn() {
